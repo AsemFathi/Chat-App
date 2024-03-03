7b8b9c7c3587fcaec745a1f15e6909488820c295
AsemFathigit config --global user.email Asemfathi12@gmail.cogit config --global user.name AsemFathi <Asemfathi12@gmail.com>
2024-03-03 20:31:42 +0200

    Add Logout EndPoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -82,5 +82,12 @@ export const Login = async (req, res) => {
 
 export const Logout = (req, res) => {
     console.log("Logout Func");
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error", error.message);
+        res.status(500).json({ error: "internal error" });
+    }
 }
 
