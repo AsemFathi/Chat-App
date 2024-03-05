b164f91e844b28e8c2caf713db48d538b9ec5376
AsemFathigit config --global user.email Asemfathi12@gmail.cogit config --global user.name AsemFathi <Asemfathi12@gmail.com>
2024-03-05 21:17:29 +0200

    frontend part completed

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,34 @@
+import Conversation from "./Conversation"
+
+function Conversations() {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+    </div>
+  )
+}
+
+export default Conversations
+/*
+STARTER CODE FOR THIS CODE
+import Conversation from "./Conversation"
+
+function Conversations() {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+    </div>
+  )
+}
+
+export default Conversations */
