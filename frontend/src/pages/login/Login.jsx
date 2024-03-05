8cd1baaac5d6c90b4ef15016baed2930698ca823
AsemFathigit config --global user.email Asemfathi12@gmail.cogit config --global user.name AsemFathi <Asemfathi12@gmail.com>
2024-03-06 01:48:41 +0200

    Add SignUp and Logout Functionality

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const Login = () => {
   return <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
         <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -21,9 +23,9 @@ const Login = () => {
                     <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10"/>
                 </div>
 
-                <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+                <Link to="/signup" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
                     {"Don't"} have an account
-                </a>
+                </Link>
 
                 <div>
                     <button className="btn btn-block btn-sm mt-2">Login</button>
