068b20f5f4fc563e89af8becf00baa1a17c2adc5
AsemFathigit config --global user.email Asemfathi12@gmail.cogit config --global user.name AsemFathi <Asemfathi12@gmail.com>
2024-03-06 14:53:22 +0200

    Add login functionality

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -13,7 +13,7 @@ const SignUp = () => {
     gender: ''
   });
 
-  const {signup} =useSignup();
+  const {loading, signup} =useSignup();
   const handleCheckboxChange = (gender) => {
     setInputs({...inputs, gender});
   }
@@ -79,7 +79,9 @@ const SignUp = () => {
 
 
       <div>
-          <button className="btn btn-block btn-sm mt-2 border border-slate-700">Sign Up</button>
+          <button className="btn btn-block btn-sm mt-2 border border-slate-700"disabled={loading}>
+            {loading ? <span className="loading loading-spinner"></span> : "Sign Up"}
+            </button>
       </div>
       </form>
   </div>
